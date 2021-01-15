99c17bb1662c0be43416c285a8a0a731761ead47
EgeBoraErguney <egeboraerguney@gmail.com>
2021-01-15 14:03:21 +0300

    tests added

--- BigMacIndexes/tests.py
+++ BigMacIndexes/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+
+class ResponseTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_table(self):
+        response = self.client.get('/table/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_graph(self):
+        response = self.client.get('/graph/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_calculator(self):
+        response = self.client.get('/calculator/')
+        self.assertEqual(response.status_code, 200)
--- Burgernomists/settings.py
+++ Burgernomists/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8jrtnj87&ofg63ij71g=rs4^syn(!&v1jx*8*mu7lyldzuu@&!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
